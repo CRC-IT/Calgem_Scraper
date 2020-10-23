e11f10c0b011c2b5470f07db6c0e824ab3a10661
hasunhkhan <hasunkhan123@gmail.com>
2020-10-23 02:18:07 -0700

    Initial versus get_api_data

---
+++ main.py
@@ -0,0 +1,100 @@
+# This is the first draft of the calgem scraper
+import re
+import urllib.request
+
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+
+#creating a dictionary for results
+def Convert(lst1, lst2):
+    results = {lst1[i]: lst2[i] for i in range(0, len(lst1), 1)}
+    return results
+
+#get header function
+def get_api_data():
+
+    #initializing url and soup
+    http = urllib3.PoolManager()
+    url = 'https://secure.conservation.ca.gov/WellSearch/Details?api=02927040&District=&County=029&Field=228&Operator=&Lease=&APINum=&address=&ActiveWell=true&ActiveOp=true&Location=&sec=&twn=&rge=&bm=&PgStart=0&PgLength=10&SortCol=6&SortDir=asc&Command=Search'
+    response = http.request('GET', url)
+    soup = BeautifulSoup(response.data, "html.parser")
+
+    #pulls elems of the header
+    job_elems = soup.find_all(class_="panel-body")
+    #test = job_elems[0]
+
+    #removing br from the header
+    #for linebreak in soup.find_all('br'):
+    #    linebreak.extract()
+
+    #test print
+    #print(test.prettify())
+
+    #storing API
+    #api = test.find(id="wellAPI").get_text()
+    #print(api)
+
+    #storing Lease
+    #lease = test.find(class_="col-sm-3").get_text()
+    #lease = lease.strip('\n')
+    #lease = lease.replace("Lease", "")
+    #lease = lease.replace(" ","")
+    #lease = lease.replace("                        ", "")
+    #lease = lease.replace("                    ", "")
+    #print(lease)
+
+    #Well number
+
+    #================================================= URL STUFF ======================================================#
+
+    #getting url from the pdf
+    sep = 'target'
+    soup.find_all("a", href=re.compile(r"_DATA_"))
+    url = str(soup.find_all("a", href=lambda href: href and "_DATA" in href))
+    url = url.strip("[<a href=" "")
+    url = url.split(sep, 1)[0]
+    url = url.replace("\"", "")
+    #print(url)
+
+    #download the pdf
+    #urllib.request.urlretrieve(url, "test.pdf")
+
+    #collecting all labels from the file
+    labels = []
+    for label_list in soup.select('label'):
+        labels.append(label_list.string)
+        #print(labels)
+
+    #collect all data fields from the file
+    data_elems = soup.find_all(class_=["col-sm-1","col-sm-2","col-sm-3","col-sm-4","col-sm-5"])
+    data_fields = []
+    data = []
+    for data_elem in data_elems:
+        data_fields.append(re.sub('<[^>]+>', '', str(data_elem)))
+        data = [sub.replace('\n', '').replace('\r', '') for sub in data_fields]
+
+
+    #removing labels from collected data fields
+    new_strings = []
+    for x in data:
+        #this is ugly but gets the job done find a better solution long-term
+        new_string = x.replace("API #", "").replace("Lease","").replace("Well #","").replace("County", "").replace("District", "").replace("Operator", "").replace("Field", "").replace("Area", "").replace("Section","").replace("Township","").replace("Range","").replace("Base Meridian","").replace("Well Status","").replace("Pool WellTypes","").replace("SPUD Date","").replace("GIS Source","").replace("Datum","").replace("Latitude","").replace("Longitude","")
+        new_string = new_string.strip()
+        new_strings.append(new_string)
+    print(new_strings)
+    print(labels)
+
+    #creating a dictionary that maps data fields to the labels
+    print(Convert(labels,new_strings))
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_api_data()
+
+
+#assign variables to data from each field <--DONE
+#have a function to easily print header data <-- DONE
+#download the pdf to a given folder <-- DONE
+#collect all labels from header info and store them in an array <-- DONE
+
